2a18074f751032ecd286e265132b7717e987b627
Tony Jacob <tony@example.com>
2026-02-02 16:05:54 -0600

    fixed credentialsID name

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub_repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t tonyjacob79/java-app:java-mvn-1.2 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push tonyjacob79/java-app:java-mvn-1.2'
