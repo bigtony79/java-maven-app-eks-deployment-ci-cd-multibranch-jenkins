52f2deb0aea99a115561ea9f1eae090ddad8d9cb
Tony Jacob <tony@example.com>
2026-02-03 11:18:28 -0600

    Updated dockerhub credentials block

--- script.groovy
+++ script.groovy
@@ -5,8 +5,8 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t tonyjacob79/java-app:java-mvn-1.2 .'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-pat', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t tonyjacob79/java-app:java-mvn-1.3 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push tonyjacob79/java-app:java-mvn-1.2'
     }
